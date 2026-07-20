1f5f490f831b8b675a239a465e5e5a3cd37b3b49
Financial Routine <danieljunior@arttburger.com.br>
2026-07-20 13:07:14 -0300

    feat(despesas): termometro de reputacao de pagador baseado nos ultimos 30 dias

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Mantém o app funcionando offline e dispara notificações de tarefas
 // mesmo com a aba em segundo plano
 
-var CACHE_NAME = 'djf-cache-v14';
+var CACHE_NAME = 'djf-cache-v15';
 var ASSETS = [
   '/',
   '/index.html',
